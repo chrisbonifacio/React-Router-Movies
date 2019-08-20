99807ea74d95dbddda825043a2333b6b25b2c00d
Chris Bonifacio <christopher.bonifacio@gmail.com>
2019-08-19 20:56:47 -0400

    Wrapped app with router, made routes to MovieList and Movie components

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const MovieCard = props => {
-  return;
+  return <Link to={`/movies/${movie.id}`} />;
 };
 
 export default MovieCard;
